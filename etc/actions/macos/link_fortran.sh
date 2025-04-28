d41717650ed0dfb2111ab66911722fa4741b4642
LEGRAND Simon <simon.legrand@inria.fr>
2025-04-28 18:02:18 +0200

    Automatic detection of most recent version of gfortran

--- etc/actions/macos/link_fortran.sh
+++ etc/actions/macos/link_fortran.sh
@@ -10,4 +10,8 @@ if [ "$ARCH" = "arm" ]; then
 	LOCAL_PATH=/opt/homebrew
 fi
 
-sudo ln -fs "$LOCAL_PATH/bin/gfortran-14" "$LOCAL_PATH/bin/gfortran"
+MAX_GFORTRAN_VERSIONS=$(printf '%s\n' $LOCAL_PATH/bin/gfortran-[0-9]* 2>/dev/null \
+         | sort -V \
+         | tail -n1)
+
+sudo ln -fs "$MAX_GFORTRAN_VERSIONS" "$LOCAL_PATH/bin/gfortran"
